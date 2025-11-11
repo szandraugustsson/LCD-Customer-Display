6c7814daa8ac86e4605413e8373021c536a0f15d
SADMQT <pederpontare@gmail.com>
2025-11-11 09:43:07 +0100

    push-2

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include "lcd.h"
 #include "uart.h"
 
@@ -16,36 +17,120 @@
 #define BIT_SET(a, b) ((a) |= (1ULL << (b)))
 #define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
 #define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
-#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b)))) 
+#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b))))
 
 
 #define BUTTON_IS_CLICKED(PINB,BUTTON_PIN) !BIT_CHECK(PINB,BUTTON_PIN) // knapp/input
-//----------------------------------------------------------------------------------
+
+//--------------------------------STRUCTS--------------------------------------
 typedef struct       
 {
-char AdMessage[32];      // Själva reklammeddelande
+    char AdMessage[32];      // Själva reklammeddelande
 }
 Message;                  // Struct - varje instans innehåller 1 reklammeddelande
 
 typedef struct 
 {
-char name[20];        //kundnamn
-Message messages[3];  // Antal Ads kund betalat för (max 3 Ads)
-int messageCount;     // iterationsvariabel för antal Ads given kund är tilldelad
-int paid;             // Total summa kund har betalat
+    char name[20];        //kundnamn
+    Message messages[3];  // Antal Ads kund betalat för (max 3 Ads)
+    int messageCount;     // iterationsvariabel för antal Ads given kund är tilldelad
+    int paid;             // Total summa kund har betalat
 }
 Customer;      // Struct - varje instans innehåller 1 kund
 
+//--------------------------------FUNTIONS--------------------------------------
+void CGRAM(char *InStr)
+{
+    int j = 0;
+    char UtStr[strlen(InStr)];
+    memset(UtStr,0,sizeof(UtStr));
+    for (int i = 0; i < strlen(InStr); i++)
+    {   
+    if      ((uint8_t)InStr[i] == 0xC3 && (uint8_t)InStr[i+1] == 0x85) {UtStr[j] = 1;j++; i++;}
+    else if ((uint8_t)InStr[i] == 0xC3 && (uint8_t)InStr[i+1] == 0x84) {UtStr[j] = 2;j++; i++;}
+    else if ((uint8_t)InStr[i] == 0xC3 && (uint8_t)InStr[i+1] == 0x96) {UtStr[j] = 3;j++; i++;}
+    else if ((uint8_t)InStr[i] == 0xC3 && (uint8_t)InStr[i+1] == 0xA5) {UtStr[j] = 4;j++; i++;}
+    else if ((uint8_t)InStr[i] == 0xC3 && (uint8_t)InStr[i+1] == 0xA4) {UtStr[j] = 5;j++; i++;}
+    else if ((uint8_t)InStr[i] == 0xC3 && (uint8_t)InStr[i+1] == 0xB6) {UtStr[j] = 6;j++; i++;}
+    else
+    {UtStr[j] = InStr[i];j++;} 
+    }
+    memset(InStr,0,sizeof(InStr));
+    strcpy(InStr,UtStr);
+}
 
+void ScrollL2R(HD44780 &lcd,char *inputStrang)
+{
+    CGRAM(inputStrang);
+    int strangLen = strlen(inputStrang);
+    char strang[strangLen+1];
+    strcpy(strang,inputStrang);
+    
+    char strangCopy[sizeof(strang)];
+    memset(strangCopy,0,sizeof(strang));
+    strcpy(strangCopy,strang);
+    
+    char strangLtR[sizeof(strang)];
+    memset(strangLtR,0,sizeof(strang));
+    
+    int i = sizeof(strang)-1;
+    int cnt = 1;
+    
+    while(1)
+    {
+        if(cnt<sizeof(strang))
+        {   
+            strangLtR[0] = strang[i];
+            lcd.WriteTextTopRow(strangLtR);
+            memmove(strangLtR + 1, strangLtR, sizeof(strangLtR)-1);
+            _delay_ms(500);
+            lcd.Clear();                      
+            i--;
+            cnt++;
+        }
+        else if(cnt>=sizeof(strang))
+        {        
+            strangCopy[sizeof(strangCopy)-1] = '\0';
+            lcd.WriteTextTopRow(strangCopy);
+            _delay_ms(500);
+            lcd.Clear();
+            memmove(strangCopy + 1, strangCopy, sizeof(strangCopy)-1);
+            strangCopy[i] = ' ';
+            i++;
+            if(i==sizeof(strang))
+            {
+                strcpy(strangCopy,strang);
+                memset(strangLtR,0,sizeof(strang));
+                i = sizeof(strang)-1;
+                cnt=1;
+            }
+        }
+    }
+}
 
+//--------------------------------MAIN--------------------------------------
 int main(void)
 {
-    Customer customers[5]; // array "customers" innehåller 1 element per kund
+    init_serial();
+    HD44780 lcd;
+    lcd.Initialize(); // Initialize the LCD
+    lcd.Clear();      // Clear the LCD
+    uint8_t AW[8] = { 0b00100,0b00000,0b01110,0b10001,0b11111,0b10001,0b00000,0b10001 };
+    uint8_t AE[8] = { 0b01010,0b00000,0b01110,0b10001,0b11111,0b10001,0b00000,0b10001 };
+    uint8_t OO[8] = { 0b01010,0b00000,0b01110,0b10001,0b10001,0b10001,0b01110,0b00000 };
+    uint8_t aw[8] = { 0b00100,0b01010,0b00100,0b01110,0b00001,0b01111,0b10001,0b01111 };
+    uint8_t ae[8] = { 0b01010,0b00000,0b01110,0b00001,0b01111,0b10001,0b01111,0b00000 };
+    uint8_t oo[8] = { 0b01010,0b00000,0b01110,0b10001,0b10001,0b10001,0b01110,0b00000 };
+    
+    lcd.CreateChar(1, AW); lcd.CreateChar(2, AE); lcd.CreateChar(3, OO);
+    lcd.CreateChar(4, aw); lcd.CreateChar(5, ae); lcd.CreateChar(6, oo);
 
+    Customer customers[5]; // array "customers" innehåller 1 element per kund
+    
     //strcpy(customers[0].name, "Hederlige Harrys Bilar");
     customers[0].paid = 5000; // 1:a element i array "customers" tilldelas 5000 "lotter"    / Tot. 14500 lotter
     customers[0].messageCount = 3; // etablerar hur många Ads kund tilldelats
-
+    
     // varje element i .messages representerar 1 av max 3 Ad-meddelande 
     strcpy(customers[0].messages[0].AdMessage,"Köp bil hos Harry");            // 0-1666
     strcpy(customers[0].messages[1].AdMessage,"En god bilaffär (för Harry!)"); // 1667-3332
@@ -61,82 +146,32 @@ int main(void)
    //strcpy(customers[2].name, "Svarte Petters Svartbyggen");
     customers[2].paid = 1500; 
     customers[2].messageCount = 2;
-                  
+    
     strcpy(customers[2].messages[0].AdMessage,"Låt Petter bygga åt dig");  // 8000-8749
     strcpy(customers[2].messages[1].AdMessage,"Bygga svart? Ring Petter"); // 8750-9499
-
+    
     //strcpy(customers[3].name, "Långbens detektivbyrå");
     customers[3].paid = 4000;                   
     customers[3].messageCount = 2;
-             
+    
     strcpy(customers[3].messages[0].AdMessage,"Mysterier? Ring Långben");  // 9500-11499
     strcpy(customers[3].messages[1].AdMessage,"Långben fixar biffen");     // 11500-13499
-
-   //strcpy(customers[4].name, "IOT:s Reklambyrå");
+    
+    //strcpy(customers[4].name, "IOT:s Reklambyrå");
     customers[4].paid = 1000;                         
     customers[4].messageCount = 1;                           
-            
+    
     strcpy(customers[4].messages[0].AdMessage,"Synas här? IOT:s Reklambyrå");  // 13500-14500   
-    init_serial();
-    HD44780 lcd;
-
-    lcd.Initialize(); // Initialize the LCD
-    lcd.Clear();      // Clear the LCD
 
     srand(time(NULL));
-
+    
     //--------------------------------------------------------------------------------
-    ScrollLtoR(customers[0].messages[0].AdMessage);
-void ScrollLtoR(char *inputChar)
-{
-    char inputChar[] = ;
-    char strang[strlen(inputChar)];
-    strcpy(strang,customers[0].messages[0].AdMessage);
+    ScrollL2R(lcd,customers[0].messages[0].AdMessage);
 
-    char strangCopy[sizeof(strang)];
-    memset(strangCopy,0,sizeof(strang));
-    strcpy(strangCopy,strang);
-
-    char strangLtR[sizeof(strang)];
-    memset(strangLtR,0,sizeof(strang));
-
-    int i = sizeof(strang)-1;
-    int cnt = 1;
-
-    while(1)
-    {
-        if(cnt<sizeof(strang))
-        {   
-            strangLtR[0] = strang[i];
-            lcd.WriteTextTopRow(strangLtR);
-            memmove(strangLtR + 1, strangLtR, sizeof(strangLtR)-1);
-            _delay_ms(500);
-             lcd.Clear();                      
-            i--;
-            cnt++;
-        }
-        else if(cnt>=sizeof(strang))
-        {        
-            strangCopy[sizeof(strangCopy)-1] = '\0';
-            lcd.WriteTextTopRow(strangCopy);
-            _delay_ms(500);
-             lcd.Clear();
-            memmove(strangCopy + 1, strangCopy, sizeof(strangCopy)-1);
-            strangCopy[i] = ' ';
-            i++;
-            if(i==sizeof(strang))
-                {
-                    strcpy(strangCopy,strang);
-                    memset(strangLtR,0,sizeof(strang));
-                    i = sizeof(strang)-1;
-                    cnt=1;
-                }
-        }
-    }
-}
+    
     // while(1)
     // {
-    //     int RandNum = rand() % 14500;
+        //     int RandNum = rand() % 14500;
     //     lcd.Clear();    
     //     if(RandNum > 0 && RandNum < 1667)                 // 0-1666
     //         {lcd.WriteText(customers[0].messages[0].AdMessage);_delay_ms(3000);}
